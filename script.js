aebcb3f2f172b9ab5493fdab2a0375525599934a
rodriguestg <grs.ibr@gmail.com>
2022-05-02 08:07:06 -0300

    subindo alterações

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function selecaoDeCor() {
   document.getElementsByClassName("selected").click()
 }
 
+function limpeza() {
+  document.getElementsByClassName("pixel").style.backgroundColor = "red";
+  document.getElementById("pixel-board").style.backgroundColor = "blue";
+}
